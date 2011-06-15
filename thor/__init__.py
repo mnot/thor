6f514e516058bfb81446f6bccc5d3c0929c6c989
Mark Nottingham <mnot@mnot.net>
2011-06-14 18:46:58 -0700

    add a bit of documentation to the package for help().

--- thor/__init__.py
+++ thor/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+Simple Event-Driven IO for Python
+
+Thor is a Python library for evented IO, with a focus on enabling high-performance HTTP intermediaries.
+"""
+
 __version__ = "0.0"
 
 from loop import run, stop, time, schedule, running
