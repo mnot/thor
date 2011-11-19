bf0c20941200fc724f3c86d79c2762ea1eb7d774
Mark Nottingham <mnot@mnot.net>
2011-11-18 20:48:03 -0800

    Bring over spdy from nbhttp

---
+++ thor/spdy/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from client import SpdyClient
+from server import SpdyServer
+
+#from common import header_names, header_dict, get_header, \
+#  safe_methods, idempotent_methods, hop_by_hop_hdrs
