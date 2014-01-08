1d210ba572390787ee8d08bfa1113353e7ac3a03
Mark Nottingham <mnot@mnot.net>
2014-01-08 16:22:02 +0800

    add pypy to travis testing

--- thor/http/__init__.py
+++ thor/http/__init__.py
@@ -4,3 +4,5 @@ from thor.http.client import HttpClient
 from thor.http.server import HttpServer
 from thor.http.common import header_names, header_dict, get_header, \
   safe_methods, idempotent_methods, hop_by_hop_hdrs
+
+debug = False
