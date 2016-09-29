d14b1fe81e3352a0dc22161aeda45e57f5118908
Mark Nottingham <mnot@mnot.net>
2016-09-29 10:11:41 +1000

    update run

--- test/run.py
+++ test/run.py
@@ -12,7 +12,7 @@ from test_events import TestEventEmitter
 from test_http_parser import TestHttpParser
 from test_http_client import TestHttpClient
 from test_http_server import TestHttpServer
-from test_http_utils import TestHttpUtils
+from test_http_utils import TestHttpUtilsBytes, TestHttpUtilsStrings
 from test_loop import TestLoop
 from test_tcp_client import TestTcpClientConnect
 from test_tcp_server import TestTcpServer
