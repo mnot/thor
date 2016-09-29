2a706f95f30d726a2a31e3a22172e084a5609cb9
Mark Nottingham <mnot@mnot.net>
2016-09-29 14:55:23 +1000

    bump version

--- thor/__init__.py
+++ thor/__init__.py
@@ -32,7 +32,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 from thor.loop import run, stop, time, schedule, running
 from thor.tcp import TcpClient, TcpServer
