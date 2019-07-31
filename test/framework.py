89e67a5ce23e78527308629c107c566a5de4df20
Mark Nottingham <mnot@mnot.net>
2019-07-31 12:25:34 -0400

    try a different test port

--- test/framework.py
+++ test/framework.py
@@ -14,7 +14,7 @@ import thor
 from thor.http.common import HttpMessageHandler, States
 
 test_host = b"127.0.0.1"
-test_port = 8001
+test_port = 9000
 
 
 class ClientServerTestCase(unittest.TestCase):
