e7c18a57bab7f895c4285ee66dd0b742ede5f279
Mark Nottingham <mnot@mnot.net>
2025-12-23 14:42:28 +1100

    Slightly more request timeout headroom

--- test/test_http_client.py
+++ test/test_http_client.py
@@ -31,7 +31,7 @@ def drain(conn, delimiter=b"\r\n\r\n"):
     due to loop scheduling). For most simple requests, a well-placed
     sendall() on the server side is sufficient to prevent flakiness.
     """
-    conn.request.settimeout(1.0)
+    conn.request.settimeout(3.0)
     data = b""
     while delimiter not in data:
         try:
